8b5927aaad11cee427acc20fdb4515c38c101aed
Alex Christenson <christenson10@gmail.com>
2021-06-18 13:57:04 -0400

    models setup

--- models/index.js
+++ models/index.js
@@ -5,22 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Products.belongsTo(Category, {
-  foreignKey: 'user_id'
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
-Category.hasMany(Products, {
-  foreignKey: 'user_id'
+Category.hasMany(Product, {
+  // foreignKey: 'user_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
-
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // as: 'product_tag',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
-
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // as: 'product_tag',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,32 +6,39 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Post.findAll({
-    attributes: [
-
+  console.log('======================');
+  Category.findAll({
+    include: [{
+      model: Product,
+      attributes: [ 
+        'id',
+        'product_name',
+        'price',
+        'stock',
+        'category_id']
       // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-    order: [[]]
-    include: [
-      {
-        model:
-        attributes: ['', '', ''],
-        include: {
-          model: 
-          attributes: ['']
-        }
-      },
-      {
-        model:
-        attributes:
-      }
-    ]
+    }],
+    // order: [['created_at', 'DESC']],
+    // include: [
+    //   {
+    //     model: Comment,
+    //     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    //     include: {
+    //       model: User,
+    //       attributes: ['username']
+    //     }
+    //   },
+    //   {
+    //     model: User,
+    //     attributes: ['username']
+    //   }
+    // ]
   })
-  .then(dbPostData => res.json(dbPostData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
@@ -39,31 +46,18 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Products
 
   //Code from module:
-  Post.findOne({
+  Category.findOne({
     where: {
       id: req.params.id
     },
     attributes: [
       'id',
-      'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'product_name',
+      'price',
+      'stock',
+      'category_id',
+      // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
   })
     .then(dbPostData => {
       if (!dbPostData) {
@@ -93,8 +87,7 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 
-  //Module code:
-  Post.update(
+  Category.update(
     {
       title: req.body.title
     },
@@ -104,12 +97,12 @@ router.put('/:id', (req, res) => {
       }
     }
   )
-  .then(dbPostData => {
-    if (!dbPostData) {
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
       res.status(404).json({ message: 'No post found with this id' });
       return;
     }
-    res.json(dbPostData);
+    res.json(dbCategoryData);
   })
   .catch(err => {
     console.log(err);
@@ -129,7 +122,7 @@ router.delete('/:id', (req, res) => {
           res.status(404).json({ message: 'No category found with this id!' });
           return;
         }
-        res.json(dbCommentData);
+        res.json(dbCategoryData);
       })
       .catch(err => {
         console.log(err);
